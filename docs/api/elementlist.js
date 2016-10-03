df2b2a28c53499e82f34bf04e76c0bcd625e55d2
Frank Bardon Jr <frankbardon@gmail.com>
2016-10-03 15:41:58 -0400

    Regenerated API documentation.

--- docs/api/elementlist.js
+++ docs/api/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Webmax\\VelmaClient\\CommandPacket"],["c","Webmax\\VelmaClient\\Exception\\BadPacketDefinitionException"],["c","Webmax\\VelmaClient\\Model\\Contact"],["c","Webmax\\VelmaClient\\Model\\VelmaResponse"],["c","Webmax\\VelmaClient\\VelmaClient"]];
+ApiGen.elements = [["c","Webmax\\VelmaClient\\CommandPacket"],["c","Webmax\\VelmaClient\\Exception\\BadPacketDefinitionException"],["c","Webmax\\VelmaClient\\Exception\\VelmaException"],["c","Webmax\\VelmaClient\\Model\\Contact"],["c","Webmax\\VelmaClient\\Model\\VelmaResponse"],["c","Webmax\\VelmaClient\\VelmaClient"]];
